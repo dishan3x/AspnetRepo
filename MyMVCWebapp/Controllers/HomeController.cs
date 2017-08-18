7df538a6b61507e23b723befe913b0934eb76e3d
dishan3x <dishan3x@gmail.com>
2017-08-18 16:46:54 -0500

    2nd commit

--- MyMVCWebapp/Controllers/HomeController.cs
+++ MyMVCWebapp/Controllers/HomeController.cs
@@ -9,14 +9,27 @@ namespace MyMVCWebapp.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        [ActionName("Default")] // when we need a diffenrenct name for action.
-        public ActionResult Index()
+        // [ActionName("Default")] // when we need a diffenrenct name for action.
+        //[HttpGet]
+       // public ActionResult Index()
+        //{
+        //    return View("Index");
+        //}
+
+        //[HttpPost]
+        //[RequireHttps] // need a http request
+        public ActionResult Index(int? id)
         {
-            return View("Index");
+            if (id.HasValue)
+                ViewData["id"] = id.Value;
+            else
+                ViewData["id"]= 0;
+            return View();
         }
 
         public ActionResult About(string id)
         {
+            int? idt = ViewBag.id;
             if (id == null) return View();
             return View("Contact");
         }
